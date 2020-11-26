6b9847af22311d7f0b3fbbccdd7669ce7977a260
Anntroy <ana.troyan@gmail.com>
2020-11-26 19:38:01 +0100

    adding level page to index.html

--- assets/script.js
+++ assets/script.js
@@ -35,3 +35,20 @@ var expertUsers = usersArray.filter((user) => user.userLevel == "expert");
 var expertResult = expertUsers.map((user) => `<div class="person_score_input"><p id="person_name_expert">${user.userName}</p>
 <p id="person_score_expert"><span class="score_count">${user.userScore}</span> clicks</p></div>`)
 expertScoreCard.innerHTML = expertResult.join(' ');
+const displayContainer = document.getElementById("display");
+const submitButton = document.getElementById("submitBtn");
+
+submitButton.addEventListener("click", goToLevelPage);
+
+function goToLevelPage () {
+    console.log(submitButton);
+    displayContainer.innerHTML = "";
+    displayContainer.innerHTML =`
+    <form class="display_container__form" action="">
+    <legend class="display_container__legend">Chose your level</legend><br>
+    <div class="display_container__div">
+        <button type="submit" class="display_container__button" id="beginers">Beginers</button>
+        <button type="submit" class="display_container__button" id="experts">Experts</button>
+    </div>
+</form>`
+};
